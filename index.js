bca9e1c8bb173addc37441badcd42d1f8103954a
Noah <noah.blanchard@etu.univ-lyon1.fr>
2021-03-21 22:54:53 +0100

    added scroll animation

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-document.querySelectorAll(".bar-container").forEach(bar => {
+const pageContent = document.getElementById('page-content');
+const pageSections = document.getElementsByClassName('page-section');
+const barContainer = document.getElementsByClassName('bar-container');
+
+pageContent.scrollTop = 0;
+
+for(let i = 0; i < barContainer.length; ++i){
+    barContainer[i].addEventListener('click', scrollContent => {
+        console.log(i);
+        
+    })
+}
+
+document.querySelectorAll(".bar-container").forEach(function(bar, i) {
     bar.addEventListener("click", addActive => {
         const activeBar = document.querySelector(".left-bar.active");
         activeBar.classList.remove("active");
-         const left = bar.firstElementChild;
-         if(left.classList.contains("active")){
+        const left = bar.firstElementChild;
+        if(left.classList.contains("active")){
              left.classList.remove("active");
-         }else{
+        }else{
              left.classList.add("active");
-         }
+        }
+    })
+
+    bar.addEventListener("click", scrollContent => {
+        pageContent.scrollTop = pageContent.scrollTop + document.getElementById(i+1).offsetTop;
     })
 })
