0493ebb9c2a3ab5b086711b75fdc99702a256770
Noah <noah.blanchard@etu.univ-lyon1.fr>
2021-03-22 20:16:09 +0100

    new idea changing color

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const barContainer = document.getElementsByClassName('bar-container');
 
 pageContent.scrollTop = 0;
 
+function switchColor(){
+    if(pageContent.classList.contains("second"))
+        pageContent.classList.remove("second");
+    else
+        pageContent.classList.add("second");
+}
+
 document.querySelectorAll(".bar-container").forEach(function(bar, i) {
     bar.addEventListener("click", addActive => {
         const activeBar = document.querySelector(".left-bar.active");
@@ -19,9 +26,11 @@ document.querySelectorAll(".bar-container").forEach(function(bar, i) {
     bar.addEventListener("click", scrollContent => {
         if(pageContent.scrollTop > document.getElementById(`s${i+1}`).offsetTop){
             pageContent.scrollTop = document.getElementById(`s${i+1}`).offsetTop;
+            switchColor();
         }else if(pageContent.scrollTop < document.getElementById(`s${i+1}`).offsetTop){
             pageContent.scrollTop = document.getElementById(`s${i+1}`).offsetTop;
+            switchColor();
         }
-
+        
     })
 })
